1b8b4954467355a8c8ac80cfee4b6932c074bdc0
Mark Gray <mark.gray@gamesys.co.uk>
2017-12-14 14:57:44 +0000

    adding all 3 fields as inputs into the databse

--- app.py
+++ app.py
@@ -16,19 +16,22 @@ def showEnterDetails():
 
 @app.route('/enterDetails',methods=['POST','GET'])
 def enterDetails():
-
+	#get details from form
 	_name = request.form['inputName']
 	_price = request.form['inputPrice']
 	_distance = request.form['inputDistance']
 
+	#send details from the form to the db
 	conn = sqlite3.connect(db)
 	c = conn.cursor()
-	c.execute("ALTER TABLE restaurants ADD {pr} string;"\
-    	.format(pr=_price))
+	c.execute("INSERT INTO restaurants (Name, Price, Distance ) VALUES (?,?,?);", (_name, _price, _distance))
 	conn.commit()
 	conn.close()
 	return json.dumps({'message':'column added successfully !'})		
 
 if __name__ == "__main__":
   app.run()
+
+
+
 
